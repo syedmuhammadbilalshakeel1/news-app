f9d5ed3d1ac61a271e05276b6af1d0da145f4a55
syed muhammad bilal <syedmuhammadbilal638@gmail.com>
2022-09-22 13:51:07 +0500

    weather app

---
+++ app.js
@@ -0,0 +1,15 @@
+let area =document.querySelector('#area');
+let date =document.querySelector('#date');
+let temprature =document.querySelector('.temprature')
+let humidity =document.querySelector('.humidity')
+let winds =document.querySelector('.winds')
+let cloude =document.querySelector('.cloude')
+
+
+
+let weatherlink = "https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=";
+let apikey= "7d64ac11a8cd4edd3f3f5ae265fa4000";
+
+
+let weatherAp ="https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=7d64ac11a8cd4edd3f3f5ae265fa4000"
+// console.log(weatherApi);
