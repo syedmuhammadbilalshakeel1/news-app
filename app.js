70bbe4ad6cd16572f8453e7ab0b75ac2154b3f39
syed muhammad bilal <syedmuhammadbilal638@gmail.com>
2022-09-22 14:19:54 +0500

    not done yet

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
 let area =document.querySelector('#area');
-let date =document.querySelector('#date');
-let temprature =document.querySelector('.temprature')
-let humidity =document.querySelector('.humidity')
-let winds =document.querySelector('.winds')
-let cloude =document.querySelector('.cloude')
+let dateIn =document.querySelector('#dateIn');
+let temprature =document.querySelector('.temprature');
+let humidity =document.querySelector('.humidity');
+let winds =document.querySelector('.winds');
+let cloude =document.querySelector('.cloude');
+
+var date =new Date();
+var days =  ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var months =  ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+var today = days[date.getDay()];
+console.log(today);
+var resentMonth = months[date.getMonth()];
+console.log(resentMonth);
 
 
+function loadOnIt(){
+let lat;
+let lon;
 
-let weatherlink = "https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=";
-let apikey= "7d64ac11a8cd4edd3f3f5ae265fa4000";
+if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition((position) => )  {
+        lat =position.coords.latitude;
+        lon = position.coords.longitude;
+      fetch('https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=7d64ac11a8cd4edd3f3f5ae265fa4000')
+      .then(response=>{
+        return response.jason();
+      })  
+      .then(data1 =>{
+        console
+      } )
+    }
+}
 
 
-let weatherAp ="https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=7d64ac11a8cd4edd3f3f5ae265fa4000"
-// console.log(weatherApi);
+}
+
+
+// let weatherlink = "https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=";
+// let apikey= "7d64ac11a8cd4edd3f3f5ae265fa4000";
+
+
+// let weatherAp ="https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=7d64ac11a8cd4edd3f3f5ae265fa4000"
+// // console.log(weatherApi);
